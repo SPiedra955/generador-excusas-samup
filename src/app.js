2fe23b040d8bef92476abd944b29dbb276a56d53
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2025-12-20 15:34:41 +0000

    exercise finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  var excuses = {
+    questions: [who, action, what, when]
+  }
+
+  function excusesGenerator(excuses) {
+    let sentence = "";
+    for (let i = 0; i < excuses.questions.length; i++) {
+      let randomWord = Math.floor(Math.random() * excuses.questions[i].length);
+      sentence += excuses.questions[i][randomWord] + ' '
+      console.log(sentence);
+    }
+    document.getElementById("excuse").innerHTML = sentence;
+  }
+
+  excusesGenerator(excuses);
+
 };
