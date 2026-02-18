8b5d26723901d97b7fa2de8170e9858ed34a90b9
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2026-02-18 21:10:06 +0100

    refactoring code

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   //write your code here
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
 
-  var excuses = {
-    questions: [who, action, what, when]
-  }
+  const unifyArrays = [].push(...who, ...action, ...what, ...when);
+  const randomIndex = (arr) => Math.floor(Math.random() * arr.length);
+  const randomWord = (arr) => arr[randomIndex];
+  console.log(randomWord(what));
+  console.log(unifyArrays);
+  // function excusesGenerator(arr) {
+  //   let sentence = "";
+  //   for (let i = 0; i < arr.questions.length; i++) {
+  //     let randomWord = Math.floor(Math.random() * arr.questions[i].length);
+  //     sentence += arr.questions[i][randomWord] + ' '
+  //     console.log(sentence);
+  //   }
+  //   document.getElementById("excuse").innerHTML = sentence;
+  // }
 
-  function excusesGenerator(arr) {
-    let sentence = "";
-    for (let i = 0; i < arr.questions.length; i++) {
-      let randomWord = Math.floor(Math.random() * arr.questions[i].length);
-      sentence += arr.questions[i][randomWord] + ' '
-      console.log(sentence);
-    }
-    document.getElementById("excuse").innerHTML = sentence;
-  }
-
-  excusesGenerator(excuses);
-
+  // excusesGenerator(excuses);
 };
